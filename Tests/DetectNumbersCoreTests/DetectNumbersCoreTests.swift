4fc30d855114a53e3e4f5239fb15eaf4381c518d
Vadim <44135514+vodemn@users.noreply.github.com>
2022-09-11 22:15:18 +0300

    decreased error percent

--- Tests/DetectNumbersCoreTests/DetectNumbersCoreTests.swift
+++ Tests/DetectNumbersCoreTests/DetectNumbersCoreTests.swift
@@ -6,7 +6,7 @@ final class DetectNumbersCoreTests: XCTestCase {
         let dataset: ((Matrix, Matrix), (Matrix, Matrix))? = loadDataset()
         if (dataset != nil) {
             let network: Core = Core(inputSize: dataset!.0.0.rows, outputSize: dataset!.1.0.rows, neurons: 10, lr: 0.4)
-            network.train(inputs: dataset!.0.0, targets: dataset!.1.0, epochs: 100)
+            network.train(inputs: dataset!.0.0, targets: dataset!.1.0, epochs: 380)
             network.test(inputs: dataset!.0.1, targets: dataset!.1.1)
         }
     }
