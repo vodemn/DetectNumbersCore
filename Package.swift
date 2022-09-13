d7b195d453572c80b2f6043e6473b66e0c294ba7
Vadim <44135514+vodemn@users.noreply.github.com>
2022-09-13 10:35:27 +0300

    added resources

--- Package.swift
+++ Package.swift
@@ -21,7 +21,14 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DetectNumbersCore",
-            dependencies: []),
+            dependencies: [],
+            resources: [
+                .process("Utils/Dataset/inputs.csv"),
+                .process("Utils/Dataset/targets.csv"),
+                .process("Utils/Generated/Errors.csv"),
+                .process("Utils/Generated/TrainedCoreValues.txt")
+            ]
+        ),
         .testTarget(
             name: "DetectNumbersCoreTests",
             dependencies: ["DetectNumbersCore"]),
